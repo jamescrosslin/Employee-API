70503f4a704ed3286d7251ce5fe5a631bd7a88f6
James Crosslin <jamesmscrosslin@gmail.com>
2021-02-04 18:19:30 -0800

    add next and previous selection in modal

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
 const url = `https://randomuser.me/api/?nat=us&exc=nat,gender,id,registered,login&results=12&noinfo`;
-const gallery = document.getElementById("gallery");
 
 class Person {
   constructor(obj, container) {
@@ -25,6 +24,7 @@ class Person {
     this.dob = date.replace(/^(\d{4})-(\d{2})-(\d{2}).+$/, "$2/$3/$1");
     this.phone = phone.replace(/^\D*(\d{3})\D*(\d{3}\D\d{4})$/, "($1) $2");
     this.container = container;
+    this.modal = null;
   }
   makeCard() {
     return `<div class="card">
@@ -64,61 +64,50 @@ class Person {
 class Gallery {
   constructor(target) {
     this.users = [];
-    this.activeModal = false;
+    this.activeProfile = null;
     this.target = target;
-  }
-  createUser(data) {
-    this.users.push(new Person(data, this));
   }
   printUsers(data) {
     data.forEach((user, i) => {
       user = new Person(user, this);
+      this.users.push(user);
       this.target.insertAdjacentHTML("beforeend", user.makeCard());
       user.element = this.target.lastElementChild;
       user.element.person = user;
-
-      // this.target.lastElementChild.addEventListener("click", () => {
-      //   const html = galleryObj.users[i].makeModal();
-      //   this.target.insertAdjacentHTML("beforeend", html);
-      //   const modal = this.target.lastElementChild;
-      //   this.activeModal = modal;
-      //   modal.addEventListener("click", (e) => {
-      //     if (e.target.tagName === "BUTTON") this.target.removeChild(modal);
-      //     if (e.target.id === "modal-prev") {
-      //       this.target.insertAdjacentHTML(
-      //         "beforeend",
-      //         galleryObj.users[
-      //           i > 0 ? i - 1 : galleryObj.users.length - 1
-      //         ].makeModal()
-      //       );
-      //     }
-      //     if (e.target.id === "modal-next") {
-      //       this.target.insertAdjacentHTML(
-      //         "beforeend",
-      //         galleryObj.users[
-      //           i < galleryObj.users.length - 1 ? i + 1 : 0
-      //         ].makeModal()
-      //       );
-      //     }
-      //   });
-      // });
     });
   }
-  printModal() {}
+  printModal(selection) {
+    this.activeProfile = selection;
+    this.target.insertAdjacentHTML("beforeend", this.activeProfile.makeModal());
+  }
+  cycleModal(button) {
+    const index = this.users.indexOf(this.activeProfile);
+    let person;
+    this.target.lastElementChild.remove();
+    if (button === "modal-prev") {
+      person = this.users[index === 0 ? this.users.length - 1 : index - 1];
+      this.printModal(person);
+    }
+    if (button === "modal-next") {
+      person = this.users[index === this.users.length - 1 ? 0 : index + 1];
+      this.printModal(person);
+    }
+  }
 }
-const galleryObj = new Gallery(document.getElementById("gallery"));
+
+const gallery = new Gallery(document.getElementById("gallery"));
 
 async function getUsers(url) {
   const res = await fetch(url);
   const { results } = await res.json();
-  galleryObj.printUsers(results);
+  gallery.printUsers(results);
 }
-getUsers(url);
-
-galleryObj.target.addEventListener("click", (e) => {
+gallery.target.addEventListener("click", (e) => {
   if (e.target.tagName === "BUTTON") {
+    gallery.cycleModal(e.target.id);
+  } else {
+    const selection = e.path[e.path.indexOf(gallery.target) - 1];
+    gallery.printModal(selection.person);
   }
-  const selection = e.path[e.path.indexOf(galleryObj.target) - 1];
-  galleryObj.activeModal = selection.person;
-  console.log(galleryObj.activeModal, selection.makeModal());
 });
+getUsers(url);
