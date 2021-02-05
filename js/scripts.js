44f96533f687e4ce661f2c8fa2bde2001039a093
James Crosslin <jamesmscrosslin@gmail.com>
2021-02-04 16:32:14 -0800

    added selection functionality

--- js/scripts.js
+++ js/scripts.js
@@ -65,50 +65,60 @@ class Gallery {
   constructor(target) {
     this.users = [];
     this.activeModal = false;
-    this.target = document.getElementById("gallery");
+    this.target = target;
   }
   createUser(data) {
     this.users.push(new Person(data, this));
   }
-  postUsers() {
-    this.users.forEach((user, i) => {
+  printUsers(data) {
+    data.forEach((user, i) => {
+      user = new Person(user, this);
       this.target.insertAdjacentHTML("beforeend", user.makeCard());
-      this.target.lastElementChild.addEventListener("click", () => {
-        const html = galleryObj.users[i].makeModal();
-        this.target.insertAdjacentHTML("beforeend", html);
-        const modal = this.target.lastElementChild;
-        this.activeModal = modal;
-        modal.addEventListener("click", (e) => {
-          if (e.target.tagName === "BUTTON") this.target.removeChild(modal);
-          if (e.target.id === "modal-prev") {
-            this.target.insertAdjacentHTML(
-              "beforeend",
-              galleryObj.users[
-                i > 0 ? i - 1 : galleryObj.users.length - 1
-              ].makeModal()
-            );
-          }
-          if (e.target.id === "modal-next") {
-            this.target.insertAdjacentHTML(
-              "beforeend",
-              galleryObj.users[
-                i < galleryObj.users.length - 1 ? i + 1 : 0
-              ].makeModal()
-            );
-          }
-        });
-      });
+      user.element = this.target.lastElementChild;
+      user.element.person = user;
+
+      // this.target.lastElementChild.addEventListener("click", () => {
+      //   const html = galleryObj.users[i].makeModal();
+      //   this.target.insertAdjacentHTML("beforeend", html);
+      //   const modal = this.target.lastElementChild;
+      //   this.activeModal = modal;
+      //   modal.addEventListener("click", (e) => {
+      //     if (e.target.tagName === "BUTTON") this.target.removeChild(modal);
+      //     if (e.target.id === "modal-prev") {
+      //       this.target.insertAdjacentHTML(
+      //         "beforeend",
+      //         galleryObj.users[
+      //           i > 0 ? i - 1 : galleryObj.users.length - 1
+      //         ].makeModal()
+      //       );
+      //     }
+      //     if (e.target.id === "modal-next") {
+      //       this.target.insertAdjacentHTML(
+      //         "beforeend",
+      //         galleryObj.users[
+      //           i < galleryObj.users.length - 1 ? i + 1 : 0
+      //         ].makeModal()
+      //       );
+      //     }
+      //   });
+      // });
     });
   }
-  postModal() {}
+  printModal() {}
 }
 const galleryObj = new Gallery(document.getElementById("gallery"));
 
 async function getUsers(url) {
   const res = await fetch(url);
   const { results } = await res.json();
-  results.forEach((user) => galleryObj.createUser(user));
-
-  galleryObj.postUsers();
+  galleryObj.printUsers(results);
 }
 getUsers(url);
+
+galleryObj.target.addEventListener("click", (e) => {
+  if (e.target.tagName === "BUTTON") {
+  }
+  const selection = e.path[e.path.indexOf(galleryObj.target) - 1];
+  galleryObj.activeModal = selection.person;
+  console.log(galleryObj.activeModal, selection.makeModal());
+});
