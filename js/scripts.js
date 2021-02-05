e6964f9386828c4ea82c910386d1e056c17c75c0
James Crosslin <jamesmscrosslin@gmail.com>
2021-02-04 20:55:41 -0800

    rewrite to accommodate future search option

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
 class Person {
-  constructor(obj, container) {
+  /**
+   * @constructor
+   * @param {object} data an object from the randomuser.me API
+   * @description creates a
+   */
+  constructor(data) {
+    //destructure the incoming object to assign values to class object properties
     const {
       name: { first, last },
       location: {
@@ -12,44 +18,37 @@ class Person {
       phone,
       email,
       picture: { large: pic },
-    } = obj;
+    } = data;
 
-    this.name = `${first} ${last}`;
-    this.image = pic;
-    this.email = email;
-    this.city = `${city}, ${state}`;
-    this.location = `${streetNum} ${streetName}<br>${city}, ${state} ${postcode}`;
-    this.dob = date.replace(/^(\d{4})-(\d{2})-(\d{2}).+$/, "$2/$3/$1");
-    this.phone = phone.replace(/^\D*(\d{3})\D*(\d{3}\D\d{4})$/, "($1) $2");
-    this.container = container;
-    this.modal = null;
-  }
-  makeCard() {
-    return `<div class="card">
+    //reformat date from "YYYY-MM-DD" to "MM/DD/YYYY"
+    const bDay = date.replace(/^(\d{4})-(\d{2})-(\d{2}).+$/, "$2/$3/$1");
+    //reformat phone number from "(xxx)-xxx-xxxx" to "(xxx) xxx-xxxx"
+    const phoneNUm = phone.replace("-", " ");
+
+    this.user = null;
+    this.card = `<div class="card show">
       <div class="card-img-container">
-        <img class="card-img" src="${this.image}" alt="profile picture">
+        <img class="card-img" src="${pic}" alt="profile picture">
       </div>
       <div class="card-info-container">
-        <h3 class="card-name cap">${this.name}</h3>
-        <p class="card-text">${this.email}</p>
-        <p class="card-text cap">${this.city}</p>
+        <h3 class="card-name cap">${first} ${last}</h3>
+        <p class="card-text">${email}</p>
+        <p class="card-text cap">${city}, ${state}</p>
       </div>
     </div>`;
-  }
-  makeModal() {
-    return `
+    this.modal = `
     <div class="modal-container">
       <div class="modal">
         <button type="button" id="modal-close-btn" class="modal-close-btn">X</button>
         <div class="modal-info-container">
-          <img class="modal-img" src="${this.image}" alt="profile picture">
-          <h3 class="modal-name cap">${this.name}</h3>
-          <p class="modal-text">${this.email}</p>
-          <p class="modal-text cap">${this.city}</p>
+          <img class="modal-img" src="${pic}" alt="profile picture">
+          <h3 class="modal-name cap">${first} ${last}</h3>
+          <p class="modal-text">${email}</p>
+          <p class="modal-text cap">$${city}, ${state}</p>
           <hr>
-          <p class="modal-text">${this.phone}</p>
-          <p class="modal-text">${this.location}</p>
-          <p class="modal-text">Birthday: ${this.dob}</p>
+          <p class="modal-text">${phoneNUm}</p>
+          <p class="modal-text">${streetNum} ${streetName}<br>${city}, ${state} ${postcode}</p>
+          <p class="modal-text">Birthday: ${bDay}</p>
         </div>
       </div>
       <div class="modal-btn-container">
@@ -61,34 +60,36 @@ class Person {
 }
 class Gallery {
   constructor(target) {
-    this.users = [];
+    this.people = [];
+    this.showing = document.getElementsByClassName("show");
     this.activeProfile = null;
     this.target = target;
   }
   printUsers(data) {
     data.forEach((user, i) => {
       user = new Person(user, this);
-      this.users.push(user);
-      this.target.insertAdjacentHTML("beforeend", user.makeCard());
+      this.people.push(user);
+      this.target.insertAdjacentHTML("beforeend", user.card);
       user.element = this.target.lastElementChild;
       user.element.person = user;
     });
   }
   printModal(selection) {
     this.activeProfile = selection;
-    this.target.insertAdjacentHTML("beforeend", this.activeProfile.makeModal());
+    this.target.insertAdjacentHTML("beforeend", this.activeProfile.modal);
   }
   cycleModal(button) {
-    const index = this.users.indexOf(this.activeProfile);
-    let person;
+    const showing = [...this.showing];
+    const index = showing.indexOf(this.activeProfile.element);
+    let element;
     this.target.lastElementChild.remove();
     if (button === "modal-prev") {
-      person = this.users[index === 0 ? this.users.length - 1 : index - 1];
-      this.printModal(person);
+      element = showing[index === 0 ? showing.length - 1 : index - 1];
+      this.printModal(element.person);
     }
     if (button === "modal-next") {
-      person = this.users[index === this.users.length - 1 ? 0 : index + 1];
-      this.printModal(person);
+      element = showing[index === showing.length - 1 ? 0 : index + 1];
+      this.printModal(element.person);
     }
   }
 }
